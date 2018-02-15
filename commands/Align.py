d9e1bf3357bb099d4750ac7d31ae46b2415dd824
ArisAg <aris.18.08.ap@gmail.com>
2018-02-14 18:50:54 -0600

    coso

--- commands/Align.py
+++ commands/Align.py
@@ -1,24 +1,32 @@
+import wpilib
 from wpilib.command import Command
 
 class Align (Command):
 
 	def __init__ (self):
-		super().__init__('Align')
+		super().__init__('Align') 
 		self.requires(self.robot.drivetrain)
 
 	def execute (self):
-		left = self.robot.drivetrain.left_ultra.getRangeMM()
+		right = self.robot.drivetrain.left_ultra.getRangeMM()
 		right = self.robot.drivetrain.right_ultra.getRangeMM()
 		
+		wpilib.DriverStation.reportWarning('left: ' + str(left), False)
+		wpilib.DriverStation.reportWarning('right: ' + str(right), False)
 		#left = self.robot.drivetrain.left_analog.getVoltage()
-		#right = self.robot.drivetrain.right_analog.getVoltage()
+		#right = self.robot.drivetrain.right_analog.getVoltage()	  
 
-		if (left >= 30 and left <= 200) & (right >= 30 and right <= 200):
+		if left >= 10 and left <= 400 and right >= 10 and right <= 400:
 			self.cancel()
-		elif left >= 30 and left <= 200:
-			self.robot.drivetrain.driveManual(1, 0)
-		elif right >= 30 and right <= 200:
+		elif left > 400 and left < 700:
 			self.robot.drivetrain.driveManual(-1, 0)
+		elif right > 400 and right < 700:
+			self.robot.drivetrain.driveManual(1, 0)
 		
 		else:
-			self.robot.drivetrain.driveManual(0, 1) #02-0.200
+			self.robot.drivetrain.driveManual(0, 0.5) #02-0.200
+
+
+
+
+#0, 400
