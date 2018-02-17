6c9ce78299623d06c07cad411889a1161f1d9ccc
Eguzkiman <eguzki@lacueva.io>
2018-02-17 02:52:47 -0600

    Change to arm

--- subsystems/Clock.py
+++ subsystems/Clock.py
@@ -2,18 +2,18 @@ import wpilib
 from wpilib.command.subsystem import Subsystem
 from commands.LiftStop import LiftStop
 
-class Clock(Subsystem):
+class Arm(Subsystem):
 	def __init__(self):
 		super().__init__()
 
-		self.clock_motor = wpilib.Jaguar(4)
+		self.arm_motor = wpilib.Jaguar(4)
 		self.claw = wpilib.Jaguar(5)
 
 	def grab(self):
-		self.clock_motor.set(1)
+		self.arm_motor.set(1)
 
 	def release(self):
-		self.clock_motor.set(-1)
+		self.arm_motor.set(-1)
 
 	def initDefaultCommand (self):
 		self.setDefaultCommand(LiftStop())
