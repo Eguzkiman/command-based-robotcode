ad15cfff40a93b9283874bd35e5516b0e93387ee
Eguzkiman <eguzki@lacueva.io>
2018-02-16 22:11:03 -0600

    holi

--- commands/Align.py
+++ commands/Align.py
@@ -20,15 +20,15 @@ class Align (Command):
 		# left = self.robot.drivetrain.left_analog.getVoltage()
 		# right = self.robot.drivetrain.right_analog.getVoltage()	  
 
-		if left >= 10 and left <= 400 and right >= 10 and right <= 400:
-			self.cancel()
-		elif left > 400 and left < 700:
-			self.robot.drivetrain.driveManual(-1, 0)
-		elif right > 400 and right < 700:
-			self.robot.drivetrain.driveManual(1, 0)
+		# if left >= 10 and left <= 400 and right >= 10 and right <= 400:
+		# 	self.cancel()
+		# elif left > 400 and left < 700:
+		# 	self.robot.drivetrain.driveManual(-1, 0)
+		# elif right > 400 and right < 700:
+		# 	self.robot.drivetrain.driveManual(1, 0)
 		
-		else:
-			self.robot.drivetrain.driveManual(0, 0.5) #02-0.200
+		# else:
+		# 	self.robot.drivetrain.driveManual(0, 0.5) #02-0.200
 
 
 
