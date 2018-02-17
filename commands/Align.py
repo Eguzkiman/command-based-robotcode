d996a1819d4cab8d9334ec90377ea16c0918a94b
Eguzkiman <eguzki@lacueva.io>
2018-02-16 23:08:37 -0600

    coso3

--- commands/Align.py
+++ commands/Align.py
@@ -6,6 +6,7 @@ class Align (Command):
 	def __init__ (self):
 		super().__init__('Align') 
 		self.requires(self.robot.drivetrain)
+		# self.max_distance = 700
 
 	def execute (self):
 		right = self.robot.drivetrain.left_ultra.getRangeMM()
@@ -17,19 +18,23 @@ class Align (Command):
 		wpilib.DriverStation.reportWarning('front: ' + str(front), False)
 
 
+
 		# left = self.robot.drivetrain.left_analog.getVoltage()
 		# right = self.robot.drivetrain.right_analog.getVoltage()	  
 
-		# if left >= 10 and left <= 400 and right >= 10 and right <= 400:
+		# if left >= 10 and left <= 400 and right >= 10 and right <= 400 and front >= 10 and front <= 400:
 		# 	self.cancel()
-		# elif left > 400 and left < 700:
+		# elif left > 400 and left < self.max_distance:
 		# 	self.robot.drivetrain.driveManual(-1, 0)
-		# elif right > 400 and right < 700:
+		# 	self.max_distance = 800
+		# elif right > 400 and right < self.max_distance:
 		# 	self.robot.drivetrain.driveManual(1, 0)
+		# 		self.max_distance = 800
+		# elif front > 400 and front < self.max_distance:
+		# 	self.robot.drivetrain.driveManual(0,0.5)
 		
 		# else:
-		# 	self.robot.drivetrain.driveManual(0, 0.5) #02-0.200
-
+		# 	pass
 
 
 
