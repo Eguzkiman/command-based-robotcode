55138cbc002d2e377376b41dab3846b232a910ad
ArisAg <aris.18.08.ap@gmail.com>
2018-02-19 10:32:13 -0600

    verde a rojo

--- x.py
+++ x.py
@@ -8,6 +8,6 @@ while True:
 	(grabbed, frame) = camera.read()
 	# frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 	print(frame)
-	frame = finder.processFrame(frame, finder.hsvGreenBoundaries)
+	frame = finder.processFrame(frame, finder.hsvRedBoundaries)
 	cv2.imshow('img', frame)
 	key = cv2.waitKey(1) & 0xFF
