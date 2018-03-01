00ae3a2feaeb213bb9efbe9cfe70a60f2ed2c470
Eguzki <eguzki@lacueva.io>
2018-03-01 10:25:58 -0600

    hotfix

--- commands/auto.py
+++ commands/auto.py
@@ -4,6 +4,7 @@ import commandbased.flowcontrol as fc
 from commands.DriveFor import DriveFor
 from commands.MoveClawFor import MoveClawFor
 from commands.Observe import Observe
+from commands.Align import Align
 
 class Auto (CommandGroup):
 
@@ -16,10 +17,12 @@ class Auto (CommandGroup):
 		@fc.IF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'left')
 		def driveLeft (self):
 			self.addSequential(DriveFor(seconds=2, direction=(0, -0.6)))
+			# self.addSequential(Align())
 
 		@fc.ELIF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'right')
 		def driveRight (self):
 			self.addSequential(DriveFor(seconds=2, direction=(0, 0.6)))
+			# self.addSequential(Align())
 
 		@fc.ELSE
 		def goForward(self):
