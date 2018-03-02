8ccc852d202b49aeda1189707472a927ba0b3370
Eguzkiman <eguzki@lacueva.io>
2018-03-02 09:06:52 -0600

    move foward autonomo

--- commands/auto.py
+++ commands/auto.py
@@ -13,25 +13,27 @@ class Auto (CommandGroup):
 		super().__init__('auto')
 
 		self.addSequential(MoveClawFor(seconds=1.5, power=-0.5))
-		self.addSequential(Observe())
+		# self.addSequential(Observe())
 
-		@fc.IF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'left')
-		def driveLeft (self):
-			self.addSequential(DriveFor(seconds=2, direction=(0, -0.6)))
-			self.addSequential(Align())
-			self.addSequential(MoveArmFor(seconds=1.5, power=1))
-			self.addSequential(MoveClawFor(seconds=1.5, power=0.5))
+		self.addSequential(DriveFor(seconds=2.5, direction=(-0.7, 0)))
 
-		@fc.ELIF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'right')
-		def driveRight (self):
-			self.addSequential(DriveFor(seconds=2, direction=(0, 0.6)))
-			self.addSequential(Align())
-			self.addSequential(MoveArmFor(seconds=1.5, power=1))
-			self.addSequential(MoveClawFor(seconds=1.2, power=0.5))
+		# @fc.IF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'left')
+		# def driveLeft (self):
+		# 	self.addSequential(DriveFor(seconds=2, direction=(0, -0.6)))
+		# 	self.addSequential(Align())
+		# 	self.addSequential(MoveArmFor(seconds=1.5, power=1))
+		# 	self.addSequential(MoveClawFor(seconds=1.5, power=0.5))
 
-		@fc.ELSE
-		def goForward(self):
-			self.addSequential(DriveFor(seconds=3, direction=(-0.7, 0)))
-			self.addSequential(Align())
-			self.addSequential(MoveArmFor(seconds=1.5, power=1))
-			self.addSequential(MoveClawFor(seconds=1.2, power=0.5))
+		# @fc.ELIF(lambda: self.robot.sd.getValue('autoDirection', 0) == 'right')
+		# def driveRight (self):
+		# 	self.addSequential(DriveFor(seconds=2, direction=(0, 0.6)))
+		# 	self.addSequential(Align())
+		# 	self.addSequential(MoveArmFor(seconds=1.5, power=1))
+		# 	self.addSequential(MoveClawFor(seconds=1.2, power=0.5))
+
+		# @fc.ELSE
+		# def goForward(self):
+		# 	self.addSequential(DriveFor(seconds=3, direction=(-0.7, 0)))
+		# 	self.addSequential(Align())
+		# 	self.addSequential(MoveArmFor(seconds=1.5, power=1))
+		# 	self.addSequential(MoveClawFor(seconds=1.2, power=0.5))
